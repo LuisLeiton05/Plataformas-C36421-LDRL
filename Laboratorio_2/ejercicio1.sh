37a05e99600aa42bd543dea0b9ad19d129353f81
LDRL05 <ldrl05@ubuntu.myguest.virtualbox.org>
2024-09-14 15:27:46 -0600

    Finalizacion de la sintaxis en la funcion y correccion del error en la linea 8.

--- Laboratorio_2/ejercicio1.sh
+++ Laboratorio_2/ejercicio1.sh
@@ -5,40 +5,38 @@ id=$1
 if [ -z "$id" ] ; then 
     echo "Proporcione el ID de un proceso."
     exit 1
-elif [ $(ps -p "$id") ] ; then  
+elif ps -p "$id" > /dev/null 2>&1 ; then  
 
     # Creamos una funcion que nos imprima la informacion.
     info_proceso() {
-        # Con los comando creados se imprime:
-        nombre=$( )
-        ID=$( )
-        PPID=$( )
-        usuario=$( )
-        CPU=$( )
-        memoria=$( )
-        estado=$( )
-        path=$( )
+        # Con los comando modificados se imprime:
+        nombre=$( ps -p "$id" -l | tail -n +2 | awk '{print $14}' )
+        ID=$( ps -p "$id" -f -l | tail -n +2 | awk '{print $4}' )
+        parent_id=$( ps -p "$id" -f -l | tail -n +2 | awk '{print $5}' )
+        usuario=$( ps -p "$id" -f -l | tail -n +2 | awk '{print $3}' )
+        CPU=$( ps -p "$id" -o %cpu | tail -n +2)
+        memoria=$( ps -p "$id" -o %mem | tail -n +2 )
+        estado=$( ps -p "$id" -f -l | tail -n +2 | awk '{print $2}' )
+        path_pro=$( readlink /proc/"$id"/exe )
 
         # Nombre del proceso.
         echo "Nombre del proceso: $nombre"
         # ID del proceso.
-        echo ""
+        echo "ID del proceso: $ID"
         # Parent Process ID.
-        echo ""
+        echo "Parent process ID: $parent_id"
         # Usuario propietario.
-        echo ""
+        echo "Usuario propietario: $usuario"
         # Porcentaje de uso del CPU.
-        echo ""
+        echo "Porcentaje de uso del CPU: $CPU"
         # Consumo de memoria.
-        echo ""
+        echo "Consumo de memoria: $memoria"
         # Estados.
-        echo ""
+        echo "Estado: $estado"
         # Path del ejecutable.
-        echo ""
+        echo "Path del ejecutable: $path_pro"
     }
 
-    ps -p "$id" -l | tail -n +2 | awk '{print $14}' # nombre
-    ps -p "$id" -f -l | tail -n +2 | awk '{print $}' # b,c,d y g
 
     # Llamamos la funcion.
     info_proceso
