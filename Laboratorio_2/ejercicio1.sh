1085a565ea3db895a43496495d73a2fcb84252fe
LDRL05 <ldrl05@ubuntu.myguest.virtualbox.org>
2024-09-14 14:44:41 -0600

    Creacion de la funcion que imprime la informacion y modificacion de los comandos.

--- Laboratorio_2/ejercicio1.sh
+++ Laboratorio_2/ejercicio1.sh
@@ -5,8 +5,43 @@ id=$1
 if [ -z "$id" ] ; then 
     echo "Proporcione el ID de un proceso."
     exit 1
-elif [ -n "$id" ] ; then  
-    
+elif [ $(ps -p "$id") ] ; then  
+
+    # Creamos una funcion que nos imprima la informacion.
+    info_proceso() {
+        # Con los comando creados se imprime:
+        nombre=$( )
+        ID=$( )
+        PPID=$( )
+        usuario=$( )
+        CPU=$( )
+        memoria=$( )
+        estado=$( )
+        path=$( )
+
+        # Nombre del proceso.
+        echo "Nombre del proceso: $nombre"
+        # ID del proceso.
+        echo ""
+        # Parent Process ID.
+        echo ""
+        # Usuario propietario.
+        echo ""
+        # Porcentaje de uso del CPU.
+        echo ""
+        # Consumo de memoria.
+        echo ""
+        # Estados.
+        echo ""
+        # Path del ejecutable.
+        echo ""
+    }
+
+    ps -p "$id" -l | tail -n +2 | awk '{print $14}' # nombre
+    ps -p "$id" -f -l | tail -n +2 | awk '{print $}' # b,c,d y g
+
+    # Llamamos la funcion.
+    info_proceso
 
 else
     echo "No existe un proceso con ese ID."
