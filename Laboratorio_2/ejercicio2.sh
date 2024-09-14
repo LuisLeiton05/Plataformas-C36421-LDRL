34386a8d6aa350ca913b1f450365595ea7eb3240
LDRL05 <ldrl05@ubuntu.myguest.virtualbox.org>
2024-09-14 17:12:22 -0600

    Se mejora el manejo de errores, por si no se encuentran id.

--- Laboratorio_2/ejercicio2.sh
+++ Laboratorio_2/ejercicio2.sh
@@ -5,23 +5,30 @@ nom_proceso=$1
 comando=$2
 
 # Verficar que se pasan los parametros y que son correctos.
-if [ -z "$inom_proceso" ] && [-z "$comando" ] ; then 
+if [ -z "$nom_proceso" ] && [ -z "$comando" ] ; then 
     echo "Proporcione tanto el nombre como el comando del proceso."
     exit 1
 elif pgrep "$nom_proceso" > /dev/null 2>&1 ; then  
 
     # Creamos una funcion que nos imprima la informacion.
     revisar_proceso() {
-        id=$( pgrep "$nom_proceso" )
+        id=$( pgrep -o "$nom_proceso" ) # Se usa -o para obtener solo el id del mas reciente.
+        # Revisar si se encontraron id.
+        if [ -z "$id" ]; then
+            echo "No se encontró ningún proceso con el nombre proporcionado."
+            exit 1
+        fi
         estado_proceso=$(ps -p "$id" -f -l | tail -n +2 | awk '{print $2}')
-        while [ -n "$inom_proceso" ] && [-n "$comando" ]; do
+
+        # Revisar periodicamente el proceso.
+        while [ -n "$nom_proceso" ] && [ -n "$comando" ]; do
 
             # Estructura de condiciones para revisar.
             if [ "$estado_proceso" = "T" ] ; then 
                 # Levantar el proceso con el comando.
                 $comando &
                 echo "El proceso estaba inactivo, por lo que fue levantado."
-                
+
             else
                 echo "El proceso se encuentra activo."
             fi
