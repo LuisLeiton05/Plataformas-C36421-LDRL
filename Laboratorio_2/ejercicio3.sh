f43b998248d3faa0f3c5d0c47393294c03d36a25
LDRL05 <ldrl05@ubuntu.myguest.virtualbox.org>
2024-09-14 20:05:36 -0600

    Se anade la parte de que el while solo registre por 30 segundos.

--- Laboratorio_2/ejercicio3.sh
+++ Laboratorio_2/ejercicio3.sh
@@ -7,15 +7,31 @@ id=$1
 if [ -z "$id" ] ; then 
     echo "Proporcione el ejecutable/id de un proceso a monitorear."
     exit 1
-elif ps -p "$id" > /dev/null 2>&1 ; then  
 
+elif ps -p "$id" > /dev/null 2>&1 ; then  
     # Creamos una funcion que lea y registre en un archivo log.
     leer_registrar() {
+        # Creamos las variables para poder llevar el tiempo, usando el comando date.
+        tiempo_inicial=$( date +%s )
+
         # Revisar periodicamente el proceso y registrar.
         while true ; do
-            
+            # Tiempo actual.
+            tiempo_actual=$( date +%s )
 
-            # Esperar 20 segundos antes de hacer el segundo registro.
+            # Calculamos el tiempo.
+            transcurrido=$( $tiempo_actual - $tiempo_inicial )
+
+            # Revisar cuando ya hayan pasado 30 segundos (lo hago asi para que a la hora de graficar sea mas comodo).
+            if [ $transcurrido -ge 30 ] ;  then
+                echo "Paso el tiempo de registro."
+                break
+            else
+                mem_cpu=$( ps -p 3707 -o %mem,%cpu | tail -n +2 )
+                
+
+            fi
+            # Esperar 5 segundos antes de hacer el segundo registro.
             sleep 5
         done
         
