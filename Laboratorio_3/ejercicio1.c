2b022dac384dfeef979170027c9ceebefef895c9
LDRL05 <ldrl05@ubuntu.myguest.virtualbox.org>
2024-09-30 15:50:50 -0600

    Se crea el archivo vacio, se agrega la funcion main y los headers files que se necesitaran.

---
+++ Laboratorio_3/ejercicio1.c
@@ -0,0 +1,10 @@
+// Codigo para la suma de las diagonales de una matriz.
+#include <stdio.h>
+// Se agrega para poder usar rand y que sea aleatoria la matriz (es recomendado en el tecer ejercicio).
+#include <stdlib.h>
+
+int main () {
+
+
+    return 0 ;
+    }
