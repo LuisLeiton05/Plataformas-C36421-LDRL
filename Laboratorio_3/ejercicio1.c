0dda1ed76a39ae5041214353d00fdd24a1d997c8
LDRL05 <ldrl05@ubuntu.myguest.virtualbox.org>
2024-09-30 15:53:49 -0600

    Se agrega la logica para declarar, rellenar e imprimir la matriz a utilizar en el calculo.

--- Laboratorio_3/ejercicio1.c
+++ Laboratorio_3/ejercicio1.c
@@ -4,7 +4,31 @@
 #include <stdlib.h>
 
 int main () {
+    // Definimos una matriz con tamano cuadrado.
+    int numero = (rand() %5) +3; // Cambiando ese valor sumando, cambia la matriz.
+    int filas = numero ;
+    int columnas = numero ;
 
+    // Declaramos la matriz (max 5x5, pero puede cambiarse), rellenamos e imrpimimos.
+    int matriz [filas][columnas];
+
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            matriz[i][j] = (rand() %10) + 1 ;
+        }
+
+    }
+
+    printf("La matriz a usar es: \n") ;
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            printf("%d ", matriz[i][j]) ;
+        }
+        printf("\n");  
+
+    }
+
+    printf("\n");
 
     return 0 ;
     }
