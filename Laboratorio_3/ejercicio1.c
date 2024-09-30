0b8fbaa070ca842ed840d614882baf3938cd9c5e
LDRL05 <ldrl05@ubuntu.myguest.virtualbox.org>
2024-09-30 16:00:45 -0600

    Se crea la funcion que calcula la suma de las diagonales.

--- Laboratorio_3/ejercicio1.c
+++ Laboratorio_3/ejercicio1.c
@@ -5,7 +5,7 @@
 
 int main () {
     // Definimos una matriz con tamano cuadrado.
-    int numero = (rand() %5) +3; // Cambiando ese valor sumando, cambia la matriz.
+    int numero = (rand() %5) +1; // Cambiando ese valor sumando, cambia la matriz.
     int filas = numero ;
     int columnas = numero ;
 
@@ -30,5 +30,24 @@ int main () {
 
     printf("\n");
 
+
+    // Hago una funcion para la suma.
+    int suma_diagonales (int matriz[filas][columnas], int numero) {
+            // Variables que van a guardar la suma de cada diagnoal.
+            int d1 = 0 ;
+            int d2 = 0 ;
+
+            for (int k = 0; k < numero; k++) {
+                d1 += matriz[k][k] ;
+                d2 += matriz [k][numero -k -1] ;
+    
+             }
+
+            return d1 + d2 ;
+
+    }
+
+    
+    printf("La suma de las diagonales es: %d \n", suma_diagonales(matriz, numero) ) ;
     return 0 ;
     }
