09fbb7f8cc64cf04e4a89180ea4b3b70b296ff68
LDRL05 <ldrl05@ubuntu.myguest.virtualbox.org>
2024-10-02 22:55:24 -0600

    Se agrega la logica de la funcion findLargestLine.

--- Laboratorio_3/ejercicio3.c
+++ Laboratorio_3/ejercicio3.c
@@ -2,12 +2,36 @@
 
 #define SIZE 5
 
-int findLargestLine(int matrix[][SIZE]) {
-   
+int findLargestLine(int matrix[SIZE][SIZE]) {
+    // Recorrer la matriz y sumar 1 por cada 1 que encuentre.
+    int contador_max = 0 ;
+    int contador = 0 ;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (matrix[i][j] == 1){
+                contador++ ; // Si es 1 lo incrementa en 1. 
+            } else {
+                if (contador > contador_max){
+                    contador_max = contador ;// Vamor actualizando cada vez que sea 0.
+                }
 
+                // Necesitamos reiniciar el contador para la siguiente iteracion.
+                contador = 0 ;
+                }
 
+            }
+
+        }
+
+        // Necesitamos verficar si la ultima iteracion es la mayor.
+        if (contador > contador_max){
+            contador_max = contador ;
+        }
+
+    return contador_max ;
 
 }
+        
 
 int main (){
     int matrix[SIZE][SIZE] = {
