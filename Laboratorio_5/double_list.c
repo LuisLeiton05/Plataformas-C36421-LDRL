03130c31a86b9fb4492117d50c139703399de274
luisdrl <ramirezluisleiton@gmail.com>
2024-11-17 16:13:41 -0600

    Se agrega la función para insertar nodo al inicio.

--- Laboratorio_5/double_list.c
+++ Laboratorio_5/double_list.c
@@ -3,13 +3,27 @@
 #include "double_list.h"
 
 // Primer se inicializa la lista.
-void inicializar_lista (DoubleList * lista) {
+void inicializar_lista (struct DoubleList * lista) {
     lista -> inicio = NULL ;
     lista -> final = NULL ;
 }
 
 // Función para insertar nodo al inicio.
 void nodo_inicio (struct DoubleList * lista, int data) {
-    Node * nuevo_nodo = (Node *)malloc(sizeof(Node));
-    
+    struct Node * nuevo_nodo = (struct Node *)malloc(sizeof(struct Node));
+    nuevo_nodo -> data = data ;
+    nuevo_nodo -> previo = NULL ;
+    nuevo_nodo -> siguiente = lista->inicio;
+
+    // Revisamos si estamos insertando al inicio.
+    if (lista->inicio != NULL) {
+        lista->inicio->previo = nuevo_nodo;
+    }
+    else {
+        lista->final = nuevo_nodo ; // Por si está vacia, este sería también el final.
+    }
+
+    lista->inicio = nuevo_nodo ; 
 }
+
+// Función para insertar nodo al final.
