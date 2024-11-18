c7057e8a0d0f9cfe25631c09f88c73a9e8b4c73d
luisdrl <ramirezluisleiton@gmail.com>
2024-11-17 20:26:58 -0600

    Se agregan las primeras funciones y casos.

---
+++ Laboratorio_5/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "double_list.h"
+
+// Definimos algunos valores a usar.
+    #define x 190 
+    #define y 25 
+    #define z 223 
+
+int main() {
+    // Inicializamos nuestra lista.
+    struct DoubleList lista ;
+    inicializar_lista(&lista) ;
+
+    // Insertamos algunos elementos en la primera posición.
+    nodo_al_inicio(&lista, x) ;
+    nodo_al_inicio(&lista, y) ;
+        // Imprimimos la lista actual y agremos otro de primero.
+        
+
+
+}
